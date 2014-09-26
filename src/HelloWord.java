2d477cc06d9ff94ae7af792626998ca342b90402
cdns <blanday@qq.com>
2014-09-26 16:17:12 +0800

    test

---
+++ src/HelloWord.java
@@ -0,0 +1,12 @@
+import com.game.logger.Logs;
+
+
+public class HelloWord {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Logs.debugLog.debug("HelloWord");
+	}
+
+}
---
+++ src/com/game/logger/Logs.java
@@ -0,0 +1,9 @@
+package com.game.logger;
+
+import org.apache.log4j.Logger;
+
+
+public class Logs {
+	public final static Logger debugLog = Logger.getLogger("DEBUGLOGGER");
+
+}
